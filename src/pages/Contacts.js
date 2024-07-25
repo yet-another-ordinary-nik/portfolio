77c60f83385f334180059fc675366338ae90cc0d
yet-another-ordinary-nik <166846396+yet-another-ordinary-nik@users.noreply.github.com>
2024-07-25 20:03:53 +0800

    Create Contacts.js

---
+++ src/pages/Contacts.js
@@ -0,0 +1,32 @@
+const Contacts = () => {
+    return (
+		<main className="section">
+			<div className="container">
+				<h1 className="title-1">Contacts</h1>
+
+				<ul className="content-list">
+					<li className="content-list__item">
+						<h2 className="title-2">Location</h2>
+						<p>Vladivostok, Russia</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Telegram</h2>
+						<p>
+							<a href="tel:+79051234567">+7 (904) 113-34-17</a>
+						</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Email</h2>
+						<p>
+							<a href="mailto:webdev@protonmail.com">
+								nikolay.nazarov.005@mail.ru
+							</a>
+						</p>
+					</li>
+				</ul>
+			</div>
+		</main>
+	);
+}
+
+export default Contacts;
