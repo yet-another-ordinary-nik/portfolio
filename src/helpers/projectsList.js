24ee1ba44dc8ee9fe70546926d30196383c7b252
yet-another-ordinary-nik <166846396+yet-another-ordinary-nik@users.noreply.github.com>
2024-07-25 20:04:20 +0800

    Create projectsList.js

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,14 @@
+import project01 from "./../img/projects/01.jpg";
+import project01Big from "./../img/projects/01-big.jpg";
+
+const projects = [
+	{
+		title: 'Calculator',
+		skills: 'React, Node.js',
+		img: project01,
+		imgBig: project01Big,
+		gitHubLink: 'https://github.com/yet-another-ordinary-nik/calc_react',
+	}
+];
+
+export {projects}
